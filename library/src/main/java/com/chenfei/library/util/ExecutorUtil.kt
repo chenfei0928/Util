97eebe55da5f5bbccbf3f3f9c57212c58f704d0f
MrFeng <chenfei0928@gmail.com>
2018-04-28 16:27:25 +0800

    使用AsyncLayoutInflater的消息线程替代HandlerThread来实现后台线程\n避免隐藏后台任务调用Looper.myLooper()带来的隐藏bug

--- library/src/main/java/com/chenfei/library/util/ExecutorUtil.kt
+++ library/src/main/java/com/chenfei/library/util/ExecutorUtil.kt
@@ -1,61 +1,109 @@
-package com.chenfei.library.util
+package com.yikelive.util
 
 import android.os.Handler
-import android.os.HandlerThread
 import android.os.Looper
-import android.os.Message
-import com.chenfei.library.util.lambdaFunction.Action1
+import android.support.annotation.MainThread
+import android.support.annotation.WorkerThread
+import android.util.Log
+import com.yikelive.retrofitUtil.RxJavaUtil
+import com.yikelive.util.lambdaFunction.Action1
+import java.util.concurrent.ArrayBlockingQueue
 
 object ExecutorUtil {
+    private const val TAG = "KW_ExecutorUtil"
     private val mMainHandler = Handler(Looper.getMainLooper())
-    private val bgHandler: Handler by lazy {
-        // 降低后台工作线程的线程优先级
-        val thread = HandlerThread("bgHandlerExecutor", 2)
-        thread.start()
-        return@lazy object : Handler(thread.looper) {
-            override fun dispatchMessage(msg: Message) {
-                try {
-                    super.dispatchMessage(msg)
-                } catch (ignore: Throwable) {
-                }
-            }
+    // 此处不要使用HandlerThread，以直接暴露其运行的Runnable调用Looper.myLooper()导致的错误
+    private val bgThread: BgExecutorThread by lazy {
+        BgExecutorThread().apply {
+            // 降低后台工作线程的线程优先级
+            priority = 2
+            start()
         }
     }
 
     @JvmStatic
-    fun <R> execute(commend: () -> R, callBack: Action1<R>) {
-        bgHandler.post {
+    fun <R> execute(@WorkerThread commend: () -> R, @MainThread callBack: Action1<R>) {
+        val throwableAction = RxJavaUtil.onError(RxJavaUtil.CELL_METHOD_STACK_TRACE + 1)
+        bgThread.enqueue(Runnable {
             try {
                 val r = commend()
                 // 结果发送给主线程
                 mMainHandler.post { callBack.call(r) }
             } catch (t: Throwable) {
+                try {
+                    throwableAction.accept(t)
+                } catch (ignore: Exception) {
+                }
             }
-        }
+        })
     }
 
     @JvmStatic
-    fun postToBg(r: Runnable): Boolean {
-        return bgHandler.post(r)
-    }
-
-    @JvmStatic
-    fun postToBgDelayed(r: Runnable, delayMillis: Long): Boolean {
-        return bgHandler.postDelayed(r, delayMillis)
+    fun postToBg(@WorkerThread r: Runnable): Boolean {
+        return bgThread.enqueue(r)
     }
 
     @JvmStatic
     fun removeBgCallbacks(r: Runnable) {
-        bgHandler.removeCallbacks(r)
+        bgThread.remove(r)
     }
 
     @JvmStatic
-    fun runOnUiThread(r: Runnable): Boolean {
+    fun runOnUiThread(@MainThread r: Runnable): Boolean {
         return mMainHandler.post(r)
     }
 
     @JvmStatic
-    fun runOnUiThreadDelayed(r: Runnable, delayMillis: Long): Boolean {
+    fun runOnUiThreadDelayed(@MainThread r: Runnable, delayMillis: Long): Boolean {
         return mMainHandler.postDelayed(r, delayMillis)
+    }
+
+    /**
+     *  参考自[android.support.v4.view.AsyncLayoutInflater.InflateThread]
+     *  移除场景不适用的 InflateRequest 对象池
+     */
+    private class BgExecutorThread : Thread("bgThreadExecutor") {
+        private val mQueue = ArrayBlockingQueue<Runnable>(10)
+
+        // Extracted to its own method to ensure locals have a constrained liveness
+        // scope by the GC. This is needed to avoid keeping previous request references
+        // alive for an indeterminate amount of time, see b/33158143 for details
+        fun runInner() {
+            val request: Runnable
+            try {
+                request = mQueue.take()
+            } catch (ex: InterruptedException) {
+                // Odd, just continue
+                Log.w(TAG, ex)
+                return
+            }
+
+            try {
+                request.run()
+            } catch (ex: RuntimeException) {
+                // Probably a Looper failure, retry on the UI thread
+                Log.w(TAG, "Failed to inflate resource in the background! Retrying on the UI" + " thread", ex)
+            }
+        }
+
+        override fun run() {
+            while (true) {
+                runInner()
+            }
+        }
+
+        fun enqueue(request: Runnable): Boolean {
+            return try {
+                mQueue.put(request)
+                true
+            } catch (e: InterruptedException) {
+                Log.e(TAG, "Failed to enqueue async inflate request", e)
+                false
+            }
+        }
+
+        fun remove(task: Runnable): Boolean {
+            return mQueue.remove(task)
+        }
     }
 }
