2bba5ed9111931cf899b7d9d3faf30e0eda7af39
MrFeng <chenfei0928@gmail.com>
2022-01-20 17:55:04 +0800

    修复在任务队列中会导致发送任务处理执行失败的bug

--- library/src/main/java/io/github/chenfei0928/os/SafeHandler.kt
+++ library/src/main/java/io/github/chenfei0928/os/SafeHandler.kt
@@ -1,8 +1,11 @@
 package io.github.chenfei0928.os
 
+import android.app.Activity
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
@@ -10,6 +13,13 @@ import io.github.chenfei0928.lifecycle.LifecycleCacheDelegate
 import io.github.chenfei0928.lifecycle.isAlive
 
 /**
+ * 与宿主生命周期绑定的Handler，会在宿主生命周期[Lifecycle.Event.ON_DESTROY]时移除所有message。
+ * 对应[Fragment]的[Fragment.onDestroy]（[Fragment.performDestroy]）,
+ * [Activity]的[Activity.onDestroy]（[FragmentActivity.onDestroy]）。
+ * 即在[Activity.onDestroy]的[super.onDestroy()]之后、[Fragment.onDestroy]、[Fragment.onDetach]中发送的任务将不会被处理。
+ *
+ * 不要在该类中重写[Handler.sendMessageAtTime]并判断生命周期忽略任务，这样会导致[Fragment.onAttach]中发送的事件不会被处理。
+ *
  * Created by MrFeng on 2017/6/28.
  */
 private class SafeHandler(
@@ -23,13 +33,6 @@ private class SafeHandler(
             return
         }
         super.dispatchMessage(msg)
-    }
-
-    override fun sendMessageAtTime(msg: Message, uptimeMillis: Long): Boolean {
-        if (!mIsAlive) {
-            return false
-        }
-        return super.sendMessageAtTime(msg, uptimeMillis)
     }
 
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
