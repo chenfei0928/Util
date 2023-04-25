52811470c779aaea1fb5c779542cce1787c3dcf9
MrFeng <chenfei0928@gmail.com>
2023-04-25 18:25:27 +0800

    更新agp 8.0

--- library/src/main/java/io/github/chenfei0928/util/StackTraceLogUtil.java
+++ library/src/main/java/io/github/chenfei0928/util/StackTraceLogUtil.java
@@ -1,7 +1,5 @@
 package io.github.chenfei0928.util;
 
-import android.text.TextUtils;
-
 import java.util.Locale;
 
 import androidx.annotation.CheckResult;
@@ -37,11 +35,9 @@ public class StackTraceLogUtil {
         String callerClazzName = caller.getClassName();
         callerClazzName = callerClazzName.substring(callerClazzName.lastIndexOf(".") + 1);
 
-        String title = String.format(Locale.getDefault(), "%s.%s(%s:%d)",
+        return String.format(Locale.getDefault(), "%s.%s(%s:%d)",
                 callerClazzName, caller.getMethodName(),
                 caller.getFileName(), caller.getLineNumber());
-        String tag = TextUtils.isEmpty(customTagPrefix) ? title : customTagPrefix + callerClazzName;
-        return tag + " " + title;
     }
 
     /**
