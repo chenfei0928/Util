15db2129c16a79e1340b7b019db654f60c537c18
MrFeng <chenfei0928@gmail.com>
2023-06-27 19:07:21 +0800

    调整suspendGetter实现，避免其初始化逻辑重复执行

--- library/src/main/java/io/github/chenfei0928/concurrent/lazy/SuspendGetter.kt
+++ library/src/main/java/io/github/chenfei0928/concurrent/lazy/SuspendGetter.kt
@@ -1,29 +1,53 @@
 package io.github.chenfei0928.concurrent.lazy
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
 import java.util.concurrent.atomic.AtomicReference
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * @author chenfei()
  * @date 2022-08-22 15:10
  */
 class SuspendGetter<T>(
-    initializer: suspend () -> T
+    context: CoroutineContext = EmptyCoroutineContext,
+    initializer: suspend CoroutineScope.() -> T
 ) {
-    private val atomicReference = AtomicReference<Any>(UNINITIALIZED_VALUE)
-    private var initializer: (suspend () -> T)? = initializer
+    private val atomicReference = AtomicReference<IGetter<T>>(
+        IGetter.DeferredGetter(context, initializer)
+    )
 
-    suspend fun get(): T {
-        val get = atomicReference.get()
-        return if (get != UNINITIALIZED_VALUE) {
-            get as T
-        } else {
-            val next = initializer!!.invoke()
-            initializer = null
-            if (atomicReference.compareAndSet(UNINITIALIZED_VALUE, next)) {
+    tailrec suspend fun get(): T = when (val mayBeDeferred = atomicReference.get()) {
+        is IGetter.Object<T> -> mayBeDeferred.obj
+        is IGetter.DeferredGetter<T> -> {
+            val next = mayBeDeferred.invoke()
+            if (atomicReference.compareAndSet(mayBeDeferred, IGetter.Object(next))) {
                 next
             } else {
-                atomicReference.get() as T
+                get()
             }
         }
+    }
+
+    private sealed interface IGetter<T> {
+        class DeferredGetter<T>(
+            context: CoroutineContext,
+            initializer: suspend CoroutineScope.() -> T
+        ) : IGetter<T>, suspend () -> T {
+            private val deferred = CoroutineScope(context).async(
+                start = CoroutineStart.LAZY,
+                block = initializer
+            )
+
+            override suspend fun invoke(): T {
+                return deferred.await()
+            }
+        }
+
+        class Object<T>(
+            val obj: T
+        ) : IGetter<T>
     }
 }
