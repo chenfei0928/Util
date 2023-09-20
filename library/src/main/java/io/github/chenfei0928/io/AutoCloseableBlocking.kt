abc65ef5e0bcad566e4b400145d598a62ca4bf0d
MrFeng <chenfei0928@gmail.com>
2023-09-20 18:55:35 +0800

    优化代码

--- library/src/main/java/io/github/chenfei0928/io/AutoCloseableBlocking.kt
+++ library/src/main/java/io/github/chenfei0928/io/AutoCloseableBlocking.kt
@@ -1,14 +1,18 @@
 package io.github.chenfei0928.io
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.CoroutineContext
 
 /**
  * 使用协程上下文启动一个阻塞代码块，并在协程上下文cancel时关闭对象
  */
-suspend fun <T : AutoCloseable> T.launchBlocking(
+suspend inline fun <T : AutoCloseable> T.launchBlocking(
     context: CoroutineContext = Dispatchers.IO,
-    block: CoroutineScope.(T) -> Unit
+    crossinline block: CoroutineScope.(T) -> Unit
 ) {
     val coroutineScope = CoroutineScope(currentCoroutineContext())
     coroutineScope.launch(context) {
