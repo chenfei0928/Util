9eb1c175bf55404efadf8d307b95593815576274
MrFeng <chenfei0928@gmail.com>
2023-10-20 16:09:48 +0800

    调整测试用例

--- library/src/test/java/io/github/chenfei0928/reflect/ParameterizedTypeReflectTest.java
+++ library/src/test/java/io/github/chenfei0928/reflect/ParameterizedTypeReflectTest.java
@@ -2,6 +2,7 @@ package io.github.chenfei0928.reflect;
 
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -24,210 +25,439 @@ public class ParameterizedTypeReflectTest {
 
     @org.junit.Test
     public void testParam() {
-        test(I.class, Object.class);
+        test(new I());
         // 之类直接实现/数组实现
-        test(I.IntArray.class, int[].class);
-        test(I.IntArrayArray.class, int[][].class);
-        test(I.IView.class, DataA.class);
+        test(new I.IntArray());
+        test(new I.IntArrayArray());
+        test(new I.IView());
         // 子类明确了泛型约束，受限于泛型擦除，只能获取到其Class
-        test(IList.class, List.class);
-        test(IList.IListView.class, (Class<List<DataA>>) ((Class) List.class));
-        test(IList.IListEmpty.class, List.class);
+        test(new IList());
+        test(new IList.IListView());
+        test(new IList.IListEmpty());
         // 数组范型，还要查找子类中该范型数组元素的具体实现
-        test(IListEArray.class, List[].class);
-        test(IListEArray.IView.class, (Class<List<DataA>[]>) ((Class) List[].class));
+        test(new IListEArray());
+        test(new IListEArray.IView());
         // 子类只实现了范型为数组（且未约束范围），但数组元素仍由范型约束由子类提供
-        test(IEArray.class, Object[].class);
-        test(IEArray.ViewArray.class, DataA[].class);
+        test(new IEArray());
+        test(new IEArray.ViewArray());
         // 子类继续范型约束该类型，要继续向子类查找该范型在子类中的实现
-        test(II.class, DataInterface.class);
-        test(II.IIViewGroup.class, DataB.class);
-        test(II.IIView.class, DataA.class);
-        test(II.Any.class, DataInterface.class);
+        test(new II());
+        test(new II.IIViewGroup());
+        test(new II.IIView());
+        test(new II.Any());
         // 中间接口测试
-        test(I1.class, List.class);
-        test(I1.ArrayList.class, (Class<ArrayList<Object>>) ((Class) ArrayList.class));
-        Class<Object> arrayListClass = new ParameterizedTypeReflect1(I1.class, I1.ArrayList.class, 1)
-                .getParentParameterizedTypeDefinedImplInChild();
+        test(new I1());
+        test(new I1.ArrayList());
+        Class<Object> arrayListClass = new ParameterizedTypeReflect1(I1.class, I1.ArrayList.class)
+                .getParentParameterizedTypeDefinedImplInChild(1);
         assertEquals(arrayListClass, ArrayList.class);
-        Class<Object> listClass = new ParameterizedTypeReflect1(I1.class, I1.class, 1)
-                .getParentParameterizedTypeDefinedImplInChild();
+        Class<Object> listClass = new ParameterizedTypeReflect1(I1.class, I1.class)
+                .getParentParameterizedTypeDefinedImplInChild(1);
         assertEquals(listClass, List.class);
         // Map
-        test(IMap.class, Map.class);
-        test(IMap.IMapIntView.class, (Class<Map<Integer, DataA>>) ((Class) Map.class));
-        test(IMap.IMapIntArrayView.class, (Class<Map<String, int[]>>) ((Class) Map.class));
-    }
-
-    private static <IInterface extends I<R>, R> void test(Class<IInterface> finalChildClass, Class<R> paramsType) {
-        ParameterizedTypeReflect1<I<R>, IInterface, R> reflect = new ParameterizedTypeReflect1(I.class, finalChildClass, 0);
-        Class<R> reflectParentParameterizedTypeDefinedImplInChild = reflect.getParentParameterizedTypeDefinedImplInChild();
-        assertEquals(reflectParentParameterizedTypeDefinedImplInChild, paramsType);
-    }
-
-    @org.junit.Test
-    public void testParam1() {
-        test1(I.class, Object.class, null);
-        // 之类直接实现/数组实现
-        test1(I.IntArray.class, int[].class, new int[]{1, 2, 3});
-        test1(I.IntArrayArray.class, int[][].class, new int[][]{{1, 2, 3}, {4, 5, 6}});
-        test1(I.IView.class, DataA.class, new DataA(5));
-        // 子类明确了泛型约束，受限于泛型擦除，只能获取到其Class
-        test1(IList.class, List.class, null);
-        test1(IList.IListView.class, (Class<List<DataA>>) ((Class) List.class),
-                CollectionsKt.arrayListOf(new DataA[]{new DataA(1), new DataA(2)})
-        );
-        test1(IList.IListEmpty.class, List.class, null);
-        // 数组范型，还要查找子类中该范型数组元素的具体实现
-        test1(IListEArray.class, List[].class, null);
-        test1(IListEArray.IView.class, (Class<List<DataA>[]>) ((Class) List[].class), new List[]{
-                CollectionsKt.arrayListOf(new DataA[]{new DataA(1), new DataA(2)}),
-                CollectionsKt.arrayListOf(new DataA[]{new DataA(2), new DataA(3)})
-        });
-        // 子类只实现了范型为数组（且未约束范围），但数组元素仍由范型约束由子类提供
-        test1(IEArray.class, Object[].class, null);
-        test1(IEArray.ViewArray.class, DataA[].class, new DataA[]{new DataA(1), new DataA(2)});
-        // 子类继续范型约束该类型，要继续向子类查找该范型在子类中的实现
-        test1(II.class, DataInterface.class, null);
-        test1(II.IIViewGroup.class, DataB.class, new DataB(2));
-        test1(II.IIView.class, DataA.class, new DataA(1));
-        test1(II.Any.class, DataInterface.class, null);
-        // 中间接口测试
-        test1(I1.class, List.class, null);
-        test1(I1.ArrayList.class, (Class<ArrayList<Object>>) ((Class) ArrayList.class), null);
-        // Map
-        test1(IMap.class, Map.class, null);
-        test1(IMap.IMapIntView.class, (Class<Map<Integer, DataA>>) ((Class) Map.class), Map.of(
-                1, new DataA(1), 2, new DataA(2)
-        ));
-        test1(IMap.IMapIntArrayView.class, (Class<Map<String, int[]>>) ((Class) Map.class), Map.of(
-                "123", new int[]{1, 2, 3}
-        ));
+        test(new IMap());
+        test(new IMap.IMapIntView());
+        test(new IMap.IMapIntArrayView());
     }
 
     private static final Gson gson = new Gson();
 
-    private static <IInterface extends I<R>, R> void test1(Class<IInterface> finalChildClass, Class<R> paramsType, R data) {
-        Type type = new ParameterizedTypeReflect1(I.class, finalChildClass, 0).getType();
-        System.out.println(finalChildClass.getName() + " : " + type);
+    private static <IInterface extends I<R>, R> void test(
+            IInterface finalChildClassInstance
+    ) {
+        Class<IInterface> finalChildClass = (Class<IInterface>) finalChildClassInstance.getClass();
+
+        ParameterizedTypeReflect1<I<R>> reflect = new ParameterizedTypeReflect1(I.class, finalChildClass);
+        Class<R> reflectParentParameterizedTypeDefinedImplInChild =
+                reflect.getParentParameterizedTypeDefinedImplInChild(0);
+        Class<R> paramsType = finalChildClassInstance.getEClass();
+        assertEquals(reflectParentParameterizedTypeDefinedImplInChild, paramsType);
+
+        Type refType = new ParameterizedTypeReflect1<>(I.class, finalChildClass).getType(0);
+        Type gsonType = finalChildClassInstance.getGsonTypeToken().getType();
+        // 此处对Xxx[]类型时二者获取到的数据不一致，不做测试断言处理，仅打印
+        // int[] 时，refType为 class [I 的虚拟机Class类
+        // gsonType为 int[][] 的GsonType创建的GenericArrayTypeImpl类
+        // 而GsonType没有对 class [I 调用equals的情况进行处理
+        // 既 com.google.gson.internal.$Gson$Types.arrayOf(int.class).equals(int[].class) 会返回 false
+        System.out.println(finalChildClass.getSimpleName() + ": " + refType + ", gson: " + gsonType);
+//        assertEquals(finalChildClassInstance.getType().getType(), type);
+
+        R data = finalChildClassInstance.getDemoDataOrNull();
         if (data != null) {
             String json = gson.toJson(data);
-            Object obj = gson.fromJson(json, (Type) type);
+            Object obj = gson.fromJson(json, (Type) refType);
             assertEquals(json, gson.toJson(obj));
         }
     }
-}
 
-interface I<E> {
-    /**
-     * 子类直接实现一阶数组 Class: Int[]
-     * <pre>
-     *     Class
-     * </pre>
-     */
-    class IntArray implements I<int[]> {
+    //<editor-fold desc="用例所用Data类" defaultstatus="collapsed">
+    static class I<E> {
+
+        TypeToken<E> getGsonTypeToken() {
+            return new TypeToken<E>() {
+            };
+        }
+
+        Class<E> getEClass() {
+            return (Class<E>) Object.class;
+        }
+
+        E getDemoDataOrNull() {
+            return null;
+        }
+
+        /**
+         * 子类直接实现一阶数组 Class: Int[]
+         * <pre>
+         *     Class
+         * </pre>
+         */
+        static class IntArray extends I<int[]> {
+            @Override
+            TypeToken<int[]> getGsonTypeToken() {
+                return new TypeToken<int[]>() {
+                };
+            }
+
+            @Override
+            Class<int[]> getEClass() {
+                return int[].class;
+            }
+
+            @Override
+            int[] getDemoDataOrNull() {
+                return new int[]{1, 2, 3};
+            }
+        }
+
+        /**
+         * 子类直接实现二阶或多阶数组 GenericArrayType
+         * <pre>
+         *     GenericArrayType
+         *         Class
+         * </pre>
+         */
+        static class IntArrayArray extends I<int[][]> {
+            @Override
+            TypeToken<int[][]> getGsonTypeToken() {
+                return new TypeToken<int[][]>() {
+                };
+            }
+
+            @Override
+            Class<int[][]> getEClass() {
+                return int[][].class;
+            }
+
+            @Override
+            int[][] getDemoDataOrNull() {
+                return new int[][]{{1, 2, 3}, {4, 5, 6}};
+            }
+        }
+
+        /**
+         * 子类直接实现泛型元素类型 (Class<?>)
+         * <pre>
+         *     Class
+         * </pre>
+         */
+        static class IView extends I<DataA> {
+            @Override
+            TypeToken<DataA> getGsonTypeToken() {
+                return new TypeToken<DataA>() {
+                };
+            }
+
+            @Override
+            Class<DataA> getEClass() {
+                return DataA.class;
+            }
+
+            @Override
+            DataA getDemoDataOrNull() {
+                return new DataA(5);
+            }
+        }
     }
 
     /**
-     * 子类直接实现二阶或多阶数组 GenericArrayType
+     * 子类的范型约束ChildP虽然是一个Interface或Class，但其仍有范型定义
+     * 但由于泛型擦除，只能获取到List.class
+     * <pre>
+     *     ParameterizedType
+     * </pre>
+     */
+    static class IList<E> extends I<List<E>> {
+
+        @Override
+        TypeToken<List<E>> getGsonTypeToken() {
+            return new TypeToken<List<E>>() {
+            };
+        }
+
+        @Override
+        Class<List<E>> getEClass() {
+            return (Class<List<E>>) ((Class) List.class);
+        }
+
+        static class IListView extends IList<DataA> {
+            @Override
+            TypeToken<List<DataA>> getGsonTypeToken() {
+                return new TypeToken<List<DataA>>() {
+                };
+            }
+
+            @Override
+            Class<List<DataA>> getEClass() {
+                return (Class<List<DataA>>) ((Class) List.class);
+            }
+
+            @Override
+            List<DataA> getDemoDataOrNull() {
+                return CollectionsKt.arrayListOf(new DataA[]{new DataA(1), new DataA(2)});
+            }
+        }
+
+        static class IListEmpty extends IList {
+            @Override
+            Class<List> getEClass() {
+                return List.class;
+            }
+        }
+    }
+
+    /**
+     * 数组范型，还要查找子类中该范型数组元素的具体实现
+     * 但由于泛型擦除，只能获取到 List[].class
      * <pre>
      *     GenericArrayType
-     *         Class
+     *         ParameterizedType
      * </pre>
      */
-    class IntArrayArray implements I<int[][]> {
+    static class IListEArray<E> extends I<List<E>[]> {
+        @Override
+        TypeToken<List<E>[]> getGsonTypeToken() {
+            return new TypeToken<List<E>[]>() {
+            };
+        }
+
+        @Override
+        Class<List<E>[]> getEClass() {
+            return (Class<List<E>[]>) ((Class) List[].class);
+        }
+
+        /**
+         * 泛型擦除，只能获取到 List[].class
+         */
+        static class IView extends IListEArray<DataA> {
+            @Override
+            TypeToken<List<DataA>[]> getGsonTypeToken() {
+                return new TypeToken<List<DataA>[]>() {
+                };
+            }
+
+            @Override
+            Class<List<DataA>[]> getEClass() {
+                return (Class<List<DataA>[]>) ((Class) List[].class);
+            }
+
+            @Override
+            List<DataA>[] getDemoDataOrNull() {
+                return new List[]{
+                        CollectionsKt.arrayListOf(new DataA[]{new DataA(1), new DataA(2)}),
+                        CollectionsKt.arrayListOf(new DataA[]{new DataA(2), new DataA(3)})
+                };
+            }
+        }
     }
 
     /**
-     * 子类直接实现泛型元素类型 (Class<?>)
-     * <pre>
-     *     Class
-     * </pre>
-     */
-    class IView implements I<DataA> {
-    }
-}
-
-/**
- * 子类的范型约束ChildP虽然是一个Interface或Class，但其仍有范型定义
- * 但由于泛型擦除，只能获取到List.class
- * <pre>
- *     ParameterizedType
- * </pre>
- */
-interface IList<E> extends I<List<E>> {
-    class IListView implements IList<DataA> {
-    }
-
-    class IListEmpty implements IList {
-    }
-}
-
-/**
- * 数组范型，还要查找子类中该范型数组元素的具体实现
- * 但由于泛型擦除，只能获取到 List[].class
- * <pre>
- *     GenericArrayType
- *         ParameterizedType
- * </pre>
- */
-interface IListEArray<E> extends I<List<E>[]> {
-    /**
-     * 泛型擦除，只能获取到 List[].class
-     */
-    class IView implements IListEArray<DataA> {
-    }
-}
-
-/**
- * 这一层子类只实现了范型为数组（且未约束范围），但数组元素仍由范型约束由子类提供
- * <pre>
- *     GenericArrayType
- *         ...
- * </pre>
- */
-interface IEArray<E> extends I<E[]> {
-    /**
-     * 中间父类只实现了范型为数组（且未约束范围），数组元素由子类提供
+     * 这一层子类只实现了范型为数组（且未约束范围），但数组元素仍由范型约束由子类提供
      * <pre>
      *     GenericArrayType
-     *         Class
+     *         ...
      * </pre>
-     * View[].class
      */
-    class ViewArray implements IEArray<DataA> {
+    static class IEArray<E> extends I<E[]> {
+        @Override
+        TypeToken<E[]> getGsonTypeToken() {
+            return new TypeToken<E[]>() {
+            };
+        }
+
+        @Override
+        Class<E[]> getEClass() {
+            return (Class) Object[].class;
+        }
+
+        /**
+         * 中间父类只实现了范型为数组（且未约束范围），数组元素由子类提供
+         * <pre>
+         *     GenericArrayType
+         *         Class
+         * </pre>
+         * View[].class
+         */
+        static class ViewArray extends IEArray<DataA> {
+            @Override
+            TypeToken<DataA[]> getGsonTypeToken() {
+                return new TypeToken<DataA[]>() {
+                };
+            }
+
+            @Override
+            Class<DataA[]> getEClass() {
+                return DataA[].class;
+            }
+
+            @Override
+            DataA[] getDemoDataOrNull() {
+                return new DataA[]{new DataA(1), new DataA(2)};
+            }
+        }
     }
-}
 
-/**
- * 子类继续范型约束该类型，要继续向子类查找该范型在子类中的实现
- * <pre>
- *     TypeVariable
- *         ...
- * </pre>
- */
-interface II<E extends DataInterface> extends I<E> {
+    /**
+     * 子类继续范型约束该类型，要继续向子类查找该范型在子类中的实现
+     * <pre>
+     *     TypeVariable
+     *         ...
+     * </pre>
+     */
+    static class II<E extends DataInterface> extends I<E> {
+        @Override
+        TypeToken<E> getGsonTypeToken() {
+            return new TypeToken<E>() {
+            };
+        }
 
-    class IIViewGroup implements II<DataB> {
+        @Override
+        Class<E> getEClass() {
+            return (Class<E>) DataInterface.class;
+        }
+
+        static class IIViewGroup extends II<DataB> {
+            @Override
+            TypeToken<DataB> getGsonTypeToken() {
+                return new TypeToken<DataB>() {
+                };
+            }
+
+            @Override
+            Class<DataB> getEClass() {
+                return DataB.class;
+            }
+
+            @Override
+            DataB getDemoDataOrNull() {
+                return new DataB(2);
+            }
+        }
+
+        static class IIView extends II<DataA> {
+            @Override
+            TypeToken<DataA> getGsonTypeToken() {
+                return new TypeToken<DataA>() {
+                };
+            }
+
+            @Override
+            Class<DataA> getEClass() {
+                return DataA.class;
+            }
+
+            @Override
+            DataA getDemoDataOrNull() {
+                return new DataA(1);
+            }
+        }
+
+        static class Any extends II {
+            @Override
+            Class getEClass() {
+                return DataInterface.class;
+            }
+        }
     }
 
-    class IIView implements II<DataA> {
+    static class I1<E, T extends List<E>> extends I<T> {
+        @Override
+        TypeToken<T> getGsonTypeToken() {
+            return new TypeToken<T>() {
+            };
+        }
+
+        @Override
+        Class<T> getEClass() {
+            return (Class<T>) ((Class) List.class);
+        }
+
+        static class ArrayList extends I1<Object, java.util.ArrayList<Object>> {
+            @Override
+            TypeToken<java.util.ArrayList<Object>> getGsonTypeToken() {
+                return new TypeToken<java.util.ArrayList<Object>>() {
+                };
+            }
+
+            @Override
+            Class<java.util.ArrayList<Object>> getEClass() {
+                return (Class<java.util.ArrayList<Object>>) ((Class) java.util.ArrayList.class);
+            }
+        }
     }
 
-    class Any implements II {
-    }
-}
+    static class IMap<K, V> extends I<Map<K, V>> {
+        @Override
+        TypeToken<Map<K, V>> getGsonTypeToken() {
+            return new TypeToken<Map<K, V>>() {
+            };
+        }
 
-interface I1<E, T extends List<E>> extends I<T> {
-    class ArrayList implements I1<Object, java.util.ArrayList<Object>> {
-    }
-}
+        @Override
+        Class<Map<K, V>> getEClass() {
+            return (Class<Map<K, V>>) ((Class) Map.class);
+        }
 
-interface IMap<K, V> extends I<Map<K, V>> {
+        static class IMapIntView extends IMap<Integer, DataA> {
+            @Override
+            TypeToken<Map<Integer, DataA>> getGsonTypeToken() {
+                return new TypeToken<Map<Integer, DataA>>() {
+                };
+            }
 
-    class IMapIntView implements IMap<Integer, DataA> {
-    }
+            @Override
+            Class<Map<Integer, DataA>> getEClass() {
+                return (Class<Map<Integer, DataA>>) ((Class) Map.class);
+            }
 
-    class IMapIntArrayView implements IMap<String, int[]> {
+            @Override
+            Map<Integer, DataA> getDemoDataOrNull() {
+                return Map.of(
+                        1, new DataA(1), 2, new DataA(2)
+                );
+            }
+        }
+
+        static class IMapIntArrayView extends IMap<String, int[]> {
+            @Override
+            TypeToken<Map<String, int[]>> getGsonTypeToken() {
+                return new TypeToken<Map<String, int[]>>() {
+                };
+            }
+
+            @Override
+            Class<Map<String, int[]>> getEClass() {
+                return (Class<Map<String, int[]>>) ((Class) Map.class);
+            }
+
+            @Override
+            Map<String, int[]> getDemoDataOrNull() {
+                return Map.of(
+                        "123", new int[]{1, 2, 3}
+                );
+            }
+        }
     }
+    //</editor-fold>
 }
