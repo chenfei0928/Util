bab9d4aca42ed7746447343e2a81e01a85b89064
MrFeng <chenfei0928@gmail.com>
2024-03-01 16:36:36 +0800

    fix(WPSClean): 修复wps文件夹清理遇到版本号无法解析问题

--- script/src/main/java/io/github0928/script/WPSClean.java
+++ script/src/main/java/io/github0928/script/WPSClean.java
@@ -1,6 +1,7 @@
 package io.github0928.script;
 
 import java.io.File;
+import java.lang.module.ModuleDescriptor;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,24 +9,24 @@ public class WPSClean {
     public static void main(String[] args) {
         var appData = System.getenv("APPDATA");
         var files = new File(appData + "\\kingsoft\\wps\\addons\\pool\\win-i386").listFiles();
-        Map<String, Map<Runtime.Version, File>> namesFile = new HashMap<>();
+        Map<String, Map<ModuleDescriptor.Version, File>> namesFile = new HashMap<>();
         for (File file : files) {
             var fileName = file.getName();
             var split = fileName.split("_");
             String name;
-            Runtime.Version version;
+            ModuleDescriptor.Version version;
             if (split.length == 2) {
                 name = split[0];
-                version = Runtime.Version.parse(split[1]);
+                version = ModuleDescriptor.Version.parse(split[1]);
             } else {
                 var versionStr = split[split.length - 1];
                 name = fileName.substring(0, fileName.length() - versionStr.length());
-                version = Runtime.Version.parse(split[split.length - 1]);
+                version = ModuleDescriptor.Version.parse(split[split.length - 1]);
             }
             if (namesFile.containsKey(name)) {
                 namesFile.get(name).put(version, file);
             } else {
-                Map<Runtime.Version, File> versions = new HashMap<>();
+                Map<ModuleDescriptor.Version, File> versions = new HashMap<>();
                 versions.put(version, file);
                 namesFile.put(name, versions);
             }
@@ -34,7 +35,7 @@ public class WPSClean {
         namesFile.forEach((name, versionFileMap) -> {
             if (versionFileMap.size() == 1)
                 return;
-            var maxVersion = versionFileMap.keySet().stream().max(Runtime.Version::compareTo);
+            var maxVersion = versionFileMap.keySet().stream().max(ModuleDescriptor.Version::compareTo);
             versionFileMap.remove(maxVersion.get());
             for (File file : versionFileMap.values()) {
                 System.out.println("delete: " + file);
