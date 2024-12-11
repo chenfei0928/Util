8736052b95afba484c4be608ec3b56eed1729eef
MrFeng <chenfei0928@gmail.com>
2024-12-11 15:27:09 +0800

    添加部分工具类

--- library/src/main/java/io/github/chenfei0928/util/Pools.kt
+++ library/src/main/java/io/github/chenfei0928/util/Pools.kt
@@ -16,3 +16,25 @@ inline fun <T : Any, R> NonnullPools<T>.use(block: (T) -> R): R {
         release(t)
     }
 }
+
+inline fun <T : Any> Pools.Pool<T>.use(block: (T?) -> Pair<T, R>): R {
+    val t = acquire()
+    var tr: Pair<T, R>? = null
+    return try {
+        tr = block(t)
+        tr.second
+    } finally {
+        (t ?: tr?.first)?.let {
+            release(it)
+        }
+    }
+}
+
+inline fun <T : Any, R> Pools.Pool<T>.use(creator: () -> T, block: (T) -> R): R {
+    val t = acquire() ?: creator()
+    return try {
+        block(t)
+    } finally {
+        release(t)
+    }
+}
