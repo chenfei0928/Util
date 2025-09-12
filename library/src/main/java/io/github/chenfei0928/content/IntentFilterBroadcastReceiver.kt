d2b57d21082e4df53ad76254bd8284a495ca7cba
MrFeng <chenfei0928@gmail.com>
2025-09-12 15:31:09 +0800

    添加IntentFilterBroadcastReceiver对LocalBroadcastManager的支持

--- library/src/main/java/io/github/chenfei0928/content/IntentFilterBroadcastReceiver.kt
+++ library/src/main/java/io/github/chenfei0928/content/IntentFilterBroadcastReceiver.kt
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import io.github.chenfei0928.lifecycle.isAlive
 
 /**
@@ -18,6 +19,11 @@ import io.github.chenfei0928.lifecycle.isAlive
 abstract class IntentFilterBroadcastReceiver(
     private vararg val actions: String
 ) : BroadcastReceiver() {
+    private val intentFilter = IntentFilter().apply {
+        for (action in actions) {
+            addAction(action)
+        }
+    }
 
     open fun register(context: Context, owner: LifecycleOwner) {
         if (!owner.lifecycle.isAlive) {
@@ -33,12 +39,25 @@ abstract class IntentFilterBroadcastReceiver(
 
     open fun register(context: Context) {
         ContextCompat.registerReceiver(
-            context, this,
-            IntentFilter().apply {
-                for (action in actions) {
-                    addAction(action)
-                }
-            }, ContextCompat.RECEIVER_EXPORTED
+            context, this, intentFilter, ContextCompat.RECEIVER_EXPORTED
         )
+    }
+
+    open fun registerLocal(context: Context, owner: LifecycleOwner) {
+        if (!owner.lifecycle.isAlive) {
+            return
+        }
+        registerLocal(context)
+        owner.lifecycle.addObserver(LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_DESTROY) {
+                LocalBroadcastManager.getInstance(context)
+                    .unregisterReceiver(this@IntentFilterBroadcastReceiver)
+            }
+        })
+    }
+
+    open fun registerLocal(context: Context) {
+        LocalBroadcastManager.getInstance(context)
+            .registerReceiver(this, intentFilter)
     }
 }
