7d249c3801209e82f8debfd98f7c9d34e7b93330
MrFeng <chenfei0928@gmail.com>
2025-11-11 11:38:12 +0800

    优化日志输出性能

--- library/src/main/java/io/github/chenfei0928/util/PrefixLogger.kt
+++ library/src/main/java/io/github/chenfei0928/util/PrefixLogger.kt
@@ -1,29 +1,31 @@
 package io.github.chenfei0928.util
 
+import androidx.annotation.Size
+
 /**
  * @author chenf()
  * @date 2025-10-29 11:30
  */
 class PrefixLogger(
-    private val prefix: String,
+    @all:Size(max = 3) private val prefix: String,
 ) : LogInterface {
-    override fun v(tag: String, msg: String, tr: Throwable?) {
+    override fun v(@Size(max = 20) tag: String, msg: String, tr: Throwable?) {
         Log.v(prefix + tag, msg, tr)
     }
 
-    override fun d(tag: String, msg: String, tr: Throwable?) {
+    override fun d(@Size(max = 20) tag: String, msg: String, tr: Throwable?) {
         Log.d(prefix + tag, msg, tr)
     }
 
-    override fun i(tag: String, msg: String, tr: Throwable?) {
+    override fun i(@Size(max = 20) tag: String, msg: String, tr: Throwable?) {
         Log.i(prefix + tag, msg, tr)
     }
 
-    override fun w(tag: String, msg: String, tr: Throwable?) {
+    override fun w(@Size(max = 20) tag: String, msg: String, tr: Throwable?) {
         Log.w(prefix + tag, msg, tr)
     }
 
-    override fun e(tag: String, msg: String, tr: Throwable?) {
+    override fun e(@Size(max = 20) tag: String, msg: String, tr: Throwable?) {
         Log.e(prefix + tag, msg, tr)
     }
 }
