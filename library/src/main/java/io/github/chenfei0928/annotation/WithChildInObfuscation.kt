0b8a3a7e1b01cf3ec9ebb7faba278f82688d743e
MrFeng <chenfei0928@gmail.com>
2026-01-15 15:19:51 +0800

    添加注释

--- library/src/main/java/io/github/chenfei0928/annotation/WithChildInObfuscation.kt
+++ library/src/main/java/io/github/chenfei0928/annotation/WithChildInObfuscation.kt
@@ -3,7 +3,7 @@ package io.github.chenfei0928.annotation
 import androidx.annotation.Keep
 
 /**
- * 使 [Keep] [KeepAllowObfuscation] 和 [KeepAllowOptimizationShrinkingObfuscation] 可以应用到子类
+ * 使 [Keep]、[KeepAllowObfuscation] 和 [KeepAllowOptimizationShrinkingObfuscation] 可以应用到子类
  *
  * @author chenf()
  * @date 2025-01-21 17:52
